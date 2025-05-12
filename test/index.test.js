49b28967db15acf2e44ecf808b8ab9fb773b2b9d
Angie <angel890817@hotmail.com>
2025-05-12 20:01:48 +1000

    feat: hello world test vreated

---
+++ test/index.test.js
@@ -0,0 +1,16 @@
+
+
+
+const {app} = require("../src/index.js");
+const request = require("supertest");
+
+describe("Make sure the serber boots up properly", () => {
+    test("Hello world routes responds with status code 200 and a nice message", async () => { 
+        let response = await request(app).get("/");
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.message).toBe("Hello Word");
+
+        
+         })
+})
